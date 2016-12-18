255a78bca4cf05715f5e240e49ebd16414e28acb
Maxim Shoustin <maxim@appsflyer.com>
2016-12-18 12:41:15 +0200

    error might happen on removing error callback

--- demo/index.ios.js
+++ demo/index.ios.js
@@ -54,11 +54,10 @@ export default class demo extends Component {
     appsFlyer.initSdk(options,
         (result) => {
           this.setState( { ...this.state, initSdkResponse: result });
+        },
+        (error) => {
+          console.error(error);
         }
-        // ,
-        // (error) => {
-        //   console.error(error);
-        // }
         )
   }
 
