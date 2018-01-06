e7fa2cf2ad42e61456586e5fe53cafbc713fa09a
Leon Mok <mok.leon@gmail.com>
2018-01-06 20:38:27 +0800

    Add linting for demo and autofix current issues

--- demo/index.android.js
+++ demo/index.android.js
@@ -3,187 +3,175 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
+import appsFlyer from 'react-native-appsflyer'
 
-import appsFlyer from 'react-native-appsflyer';
+import Button from 'react-native-button'
 
-import Button from 'react-native-button';
-
-import AfBase from './index.base';
+import AfBase from './index.base'
 
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    Linking
-} from 'react-native';
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Linking
+} from 'react-native'
 
 export default class demo extends Component {
+  constructor (props) {
+    super(props)
 
-  constructor(props) {
-    super(props);
+    this.state = {
+      initSdkResponse: 'not initialized yet',
+      gcmProjectNumberResponse: 'not called yet',
+      tokenResponse: 'not called yet'
+    }
 
-     this.state = {
-      initSdkResponse: "not initialized yet",
-      gcmProjectNumberResponse: "not called yet",
-      tokenResponse: "not called yet"
-    };
-
-    this.initSdk                    = this.initSdk.bind(this);
-    this.enableUninstallTracking    = this.enableUninstallTracking.bind(this);
-    this.updateServerUninstallToken = this.updateServerUninstallToken.bind(this);
+    this.initSdk = this.initSdk.bind(this)
+    this.enableUninstallTracking = this.enableUninstallTracking.bind(this)
+    this.updateServerUninstallToken = this.updateServerUninstallToken.bind(this)
   }
 
-
-   // Handle DeepLink URL
-  componentDidMount() {
-     Linking.getInitialURL().then((url) => {
+  // Handle DeepLink URL
+  componentDidMount () {
+    Linking.getInitialURL().then((url) => {
       if (appsFlyer) {
-        appsFlyer.sendDeepLinkData(url); // Report Deep Link to AppsFlyer
+        appsFlyer.sendDeepLinkData(url) // Report Deep Link to AppsFlyer
       }
-  }).catch(err => console.error('An error occurred', err));
+    }).catch(err => console.error('An error occurred', err))
   }
 
-
-  initSdk(){
-
+  initSdk () {
     let options = {
-      devKey:  'WdpTVAcYwmxsaQ4WeTspmh',
+      devKey: 'WdpTVAcYwmxsaQ4WeTspmh',
       isDebug: true
-    };
+    }
 
     appsFlyer.initSdk(options,
-        (result) => {
-           this.setState( { ...this.state, initSdkResponse: result });
-         },
-        (error) => {
-          console.error(error);
-        })
+      (result) => {
+        this.setState({ ...this.state, initSdkResponse: result })
+      },
+      (error) => {
+        console.error(error)
+      })
   }
 
-  enableUninstallTracking(){
-
-    const  gcmProjectNumber = "997186475229";
+  enableUninstallTracking () {
+    const gcmProjectNumber = '997186475229'
 
     appsFlyer.enableUninstallTracking(gcmProjectNumber,
-        (gcmProjectID) => {
-           this.setState( { ...this.state, gcmProjectNumberResponse: gcmProjectID });
-        })
+      (gcmProjectID) => {
+        this.setState({ ...this.state, gcmProjectNumberResponse: gcmProjectID })
+      })
   }
 
-  updateServerUninstallToken(){
-
-    const  token = "xxxxxxxxxxxxx";
+  updateServerUninstallToken () {
+    const token = 'xxxxxxxxxxxxx'
 
     appsFlyer.updateServerUninstallToken(token,
-        (response) => {
-          this.setState( { ...this.state, tokenResponse: response });
-        })
+      (response) => {
+        this.setState({ ...this.state, tokenResponse: response })
+      })
   }
 
-
-  render() {
+  render () {
     return (
-        <View style={{
-          flex: 1,
-          marginTop:10,
-          flexDirection: 'column',
-          justifyContent: 'flex-start', // 'center'
-          alignItems: 'stretch' //'flex-start' //'center'
-        }}>
+      <View style={{
+        flex: 1,
+        marginTop: 10,
+        flexDirection: 'column',
+        justifyContent: 'flex-start', // 'center'
+        alignItems: 'stretch' // 'flex-start' //'center'
+      }}>
 
-
-            <View style={styles.api_wrapper}>
-              <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
-                <Button
-                    style={styles.sdk_button}
-                    styleDisabled={{color: 'red'}}
-                    disabled={this.isDisabled}
-                    onPress={() => this.initSdk()}>
+        <View style={styles.api_wrapper}>
+          <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
+            <Button
+              style={styles.sdk_button}
+              styleDisabled={{color: 'red'}}
+              disabled={this.isDisabled}
+              onPress={() => this.initSdk()}>
                   initSDK
-                </Button>
-              </View>
-              <View style={{height: 60,  flex: 1, justifyContent: 'flex-start', alignItems: 'stretch',  backgroundColor: 'skyblue'}}>
-                <Text style={styles.json_wrap}>
-                  {this.state.initSdkResponse}
-                </Text>
-              </View>
-            </View>
-
-            <AfBase></AfBase>
-
-            <View style={styles.api_wrapper}>
-              <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
-                <Button
-                    style={styles.sdk_button}
-                    onPress={() => this.enableUninstallTracking()}>
-                  setGCMProjectID
-                </Button>
-              </View>
-              <View style={{height: 60,  flex: 1, justifyContent: 'flex-start', alignItems: 'stretch',  backgroundColor: 'skyblue'}}>
-                <Text style={styles.json_wrap}>
-                  {this.state.gcmProjectNumberResponse}
-                </Text>
-              </View>
-            </View>
-
-            <View style={styles.api_wrapper}>
-              <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
-                <Button
-                    style={styles.sdk_button}
-                    onPress={() => this.updateServerUninstallToken()}>
-                  update Uninstall Token
-                </Button>
-              </View>
-              <View style={{height: 60,  flex: 1, justifyContent: 'flex-start', alignItems: 'stretch',  backgroundColor: 'skyblue'}}>
-                <Text style={styles.json_wrap}>
-                  {this.state.tokenResponse}
-                </Text>
-              </View>
-            </View>
-
-
+            </Button>
+          </View>
+          <View style={{height: 60, flex: 1, justifyContent: 'flex-start', alignItems: 'stretch', backgroundColor: 'skyblue'}}>
+            <Text style={styles.json_wrap}>
+              {this.state.initSdkResponse}
+            </Text>
+          </View>
         </View>
 
+        <AfBase></AfBase>
 
-    );
+        <View style={styles.api_wrapper}>
+          <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
+            <Button
+              style={styles.sdk_button}
+              onPress={() => this.enableUninstallTracking()}>
+                  setGCMProjectID
+            </Button>
+          </View>
+          <View style={{height: 60, flex: 1, justifyContent: 'flex-start', alignItems: 'stretch', backgroundColor: 'skyblue'}}>
+            <Text style={styles.json_wrap}>
+              {this.state.gcmProjectNumberResponse}
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.api_wrapper}>
+          <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
+            <Button
+              style={styles.sdk_button}
+              onPress={() => this.updateServerUninstallToken()}>
+                  update Uninstall Token
+            </Button>
+          </View>
+          <View style={{height: 60, flex: 1, justifyContent: 'flex-start', alignItems: 'stretch', backgroundColor: 'skyblue'}}>
+            <Text style={styles.json_wrap}>
+              {this.state.tokenResponse}
+            </Text>
+          </View>
+        </View>
+
+      </View>
+
+    )
   }
 }
 
-AppRegistry.registerComponent('demo', () => demo);
-
+AppRegistry.registerComponent('demo', () => demo)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
     height: 60,
-    lineHeight:60
+    lineHeight: 60
   },
-  api_wrapper:{
+  api_wrapper: {
     height: 60,
-    maxHeight:60,
+    maxHeight: 60,
     flex: 1,
     flexDirection: 'row',
     borderStyle: 'solid',
     borderColor: 'black',
     borderTopWidth: 1
   },
-  sdk_button:{
+  sdk_button: {
     fontSize: 14,
     color: 'green',
     marginTop: 20,
@@ -192,7 +180,7 @@ const styles = StyleSheet.create({
     marginLeft: 10
   },
 
-  sdk_button_font_mini:{
+  sdk_button_font_mini: {
     fontSize: 12,
     color: 'green',
     marginTop: 20,
@@ -201,11 +189,11 @@ const styles = StyleSheet.create({
     marginLeft: 10
   },
 
-  json_wrap:{
-    fontSize:9,
+  json_wrap: {
+    fontSize: 9,
     marginTop: 3,
     marginBottom: 5,
     marginRight: 10,
     marginLeft: 10
   }
-});
+})
--- demo/index.ios.js
+++ demo/index.ios.js
@@ -3,160 +3,147 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import appsFlyer from 'react-native-appsflyer';
+import appsFlyer from 'react-native-appsflyer'
 
-import Button from 'react-native-button';
+import Button from 'react-native-button'
 
-import AfBase from './index.base';
+import AfBase from './index.base'
 
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
-
-
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
 
 export default class demo extends Component {
+  constructor (props) {
+    super(props)
 
-  constructor(props) {
-    super(props);
-
-    //this.isDisabled = false;
+    // this.isDisabled = false;
 
     this.state = {
-      initSdkResponse: "not initialized yet",
+      initSdkResponse: 'not initialized yet',
       trackLocation: []
-    };
+    }
 
-    this.initSdk         = this.initSdk.bind(this);
-    this.trackLocation   = this.trackLocation.bind(this);
+    this.initSdk = this.initSdk.bind(this)
+    this.trackLocation = this.trackLocation.bind(this)
   }
 
-
-
-
-  initSdk(){
-
-     let options = {
-       devKey:  'WdpTVAcYwmxsaQ4WeTspmh',
-       appId: "975313579",
-       isDebug: true
-     };
+  initSdk () {
+    let options = {
+      devKey: 'WdpTVAcYwmxsaQ4WeTspmh',
+      appId: '975313579',
+      isDebug: true
+    }
 
     appsFlyer.initSdk(options,
-        (result) => {
-          this.setState( { ...this.state, initSdkResponse: result });
-        },
-        (error) => {
-          console.error(error);
-        }
-        )
+      (result) => {
+        this.setState({ ...this.state, initSdkResponse: result })
+      },
+      (error) => {
+        console.error(error)
+      }
+    )
   }
 
-
-  trackLocation(){
-
-    const  latitude = -18.406655;
-    const  longitude = 46.406250;
+  trackLocation () {
+    const latitude = -18.406655
+    const longitude = 46.406250
 
     appsFlyer.trackLocation(longitude, latitude, (error, coords) => {
       if (error) {
-        console.error(error);
+        console.error(error)
       } else {
-        this.setState( { ...this.state, trackLocation: coords });
+        this.setState({ ...this.state, trackLocation: coords })
       }
     })
   }
 
-
-  render() {
+  render () {
     return (
-        <View style={{
-          flex: 1,
-          marginTop:40,
-          flexDirection: 'column',
-          justifyContent: 'flex-start', // 'center'
-          alignItems: 'stretch' //'flex-start' //'center'
-        }}>
+      <View style={{
+        flex: 1,
+        marginTop: 40,
+        flexDirection: 'column',
+        justifyContent: 'flex-start', // 'center'
+        alignItems: 'stretch' // 'flex-start' //'center'
+      }}>
 
-
-            <View style={styles.api_wrapper}>
-              <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
-                <Button
-                    style={styles.sdk_button}
-                    styleDisabled={{color: 'red'}}
-                    disabled={this.isDisabled}
-                    onPress={() => this.initSdk()}>
+        <View style={styles.api_wrapper}>
+          <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
+            <Button
+              style={styles.sdk_button}
+              styleDisabled={{color: 'red'}}
+              disabled={this.isDisabled}
+              onPress={() => this.initSdk()}>
                   initSDK
-                </Button>
-              </View>
-              <View style={{height: 60,  flex: 1, justifyContent: 'flex-start', alignItems: 'stretch',  backgroundColor: 'skyblue'}}>
-                <Text style={styles.json_wrap}>
-                  {this.state.initSdkResponse}
-                </Text>
-              </View>
-            </View>
-
-            <AfBase></AfBase>
-
-            <View style={styles.api_wrapper}>
-              <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
-                <Button
-                    style={styles.sdk_button}
-                    onPress={() => this.trackLocation()}>
-                  TrackLocation
-                </Button>
-              </View>
-              <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'stretch',  backgroundColor: 'skyblue'}}>
-                <Text style={styles.json_wrap}>
-                  { JSON.stringify(this.state.trackLocation, null) }
-                </Text>
-              </View>
-
-            </View>
+            </Button>
+          </View>
+          <View style={{height: 60, flex: 1, justifyContent: 'flex-start', alignItems: 'stretch', backgroundColor: 'skyblue'}}>
+            <Text style={styles.json_wrap}>
+              {this.state.initSdkResponse}
+            </Text>
+          </View>
         </View>
 
+        <AfBase></AfBase>
 
-    );
+        <View style={styles.api_wrapper}>
+          <View style={{height: 60, width: 100, backgroundColor: 'powderblue'}}>
+            <Button
+              style={styles.sdk_button}
+              onPress={() => this.trackLocation()}>
+                  TrackLocation
+            </Button>
+          </View>
+          <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'stretch', backgroundColor: 'skyblue'}}>
+            <Text style={styles.json_wrap}>
+              { JSON.stringify(this.state.trackLocation, null) }
+            </Text>
+          </View>
+
+        </View>
+      </View>
+
+    )
   }
 }
 
-AppRegistry.registerComponent('demo', () => demo);
-
+AppRegistry.registerComponent('demo', () => demo)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
     height: 60,
-    lineHeight:60
+    lineHeight: 60
   },
-  api_wrapper:{
+  api_wrapper: {
     height: 60,
-    maxHeight:60,
+    maxHeight: 60,
     flex: 1,
     flexDirection: 'row',
     borderStyle: 'solid',
     borderColor: 'black',
     borderTopWidth: 1
   },
-  sdk_button:{
+  sdk_button: {
     fontSize: 14,
     color: 'green',
     marginTop: 20,
@@ -165,7 +152,7 @@ const styles = StyleSheet.create({
     marginLeft: 10
   },
 
-  sdk_button_font_mini:{
+  sdk_button_font_mini: {
     fontSize: 12,
     color: 'green',
     marginTop: 20,
@@ -174,11 +161,11 @@ const styles = StyleSheet.create({
     marginLeft: 10
   },
 
-  json_wrap:{
-    fontSize:9,
+  json_wrap: {
+    fontSize: 9,
     marginTop: 3,
     marginBottom: 5,
     marginRight: 10,
     marginLeft: 10
   }
-});
+})
