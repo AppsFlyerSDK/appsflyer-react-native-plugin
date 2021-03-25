7ba3bd04fe3516fde2c286cccf021b761c435b3c
Amit <Amit.kremer@appsflyer.com>
2021-03-25 13:27:34 +0200

    add import bundle

--- android/src/main/java/com/appsflyer/reactnative/RNAppsFlyerModule.java
+++ android/src/main/java/com/appsflyer/reactnative/RNAppsFlyerModule.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
