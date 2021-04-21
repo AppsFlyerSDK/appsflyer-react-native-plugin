1dbe1806aacfccf2cc09f27a5d9cbdd0e10daa28
Amit <Amit.kremer@appsflyer.com>
2021-04-21 12:08:34 +0300

    remove pods

--- SampleApps/DetoxApp/ios/Pods/Target Support Files/Pods-detoxTest-detoxTestTests/Pods-detoxTest-detoxTestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_detoxTest_detoxTestTests : NSObject
-@end
-@implementation PodsDummy_Pods_detoxTest_detoxTestTests
-@end
--- SampleApps/DetoxApp/ios/Pods/Target Support Files/Pods-detoxTest/Pods-detoxTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_detoxTest : NSObject
-@end
-@implementation PodsDummy_Pods_detoxTest
-@end
