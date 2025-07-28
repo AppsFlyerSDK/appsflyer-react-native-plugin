23101ff307f61e7bd6f8c5b75d7a47d29f4babc6
Amit Levy <amit.levy@appsflyer.com>
2025-07-28 15:32:44 +0300

    Gradle modification

--- expo/withAppsFlyerAndroid.js
+++ expo/withAppsFlyerAndroid.js
@@ -1,25 +1,24 @@
+const { withGradleProperties } = require('@expo/config-plugins');
+
 module.exports = function withAppsFlyerAndroid(config, { 
-    shouldUsePurchaseConnector = false 
-  } = {}) {
-  
-    if (shouldUsePurchaseConnector) {
-      if (!config.android) {
-        config.android = {};
-      }
-  
-      if (!config.android.gradleProperties) {
-        config.android.gradleProperties = [];
-      }
-  
-      // Prevent duplicates
-      const existingKeys = config.android.gradleProperties.map(p => p.key);
-      if (!existingKeys.includes('appsflyer.enable_purchase_connector')) {
-        config.android.gradleProperties.push({
+  shouldUsePurchaseConnector = false 
+} = {}) {
+
+  if (shouldUsePurchaseConnector) {
+    config = withGradleProperties(config, config => {
+      const props = config.modResults;
+
+      const alreadySet = props.some(p => p.key === 'appsflyer.enable_purchase_connector');
+      if (!alreadySet) {
+        props.push({
           key: 'appsflyer.enable_purchase_connector',
           value: 'true',
         });
       }
-    }
-  
+
+      return config;
+    });
+  }
+
   return config;
-}; 
+};
