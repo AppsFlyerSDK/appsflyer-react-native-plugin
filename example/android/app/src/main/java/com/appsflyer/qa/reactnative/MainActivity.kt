dff744dfc2cfbe5305ca94bb6683f033785eb527
AmitLY21 <Amit.Levy@appsflyer.com>
2026-08-06 12:57:03 +0300

    patch for Android Build CI

--- example/android/app/src/main/java/com/appsflyer/qa/reactnative/MainActivity.kt
+++ example/android/app/src/main/java/com/appsflyer/qa/reactnative/MainActivity.kt
@@ -2,7 +2,6 @@ package com.appsflyer.qa.reactnative
 
 import android.content.Intent
 import android.util.Log
-import com.appsflyer.AppsFlyerLib
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,12 +18,5 @@ class MainActivity : ReactActivity() {
     super.onNewIntent(intent)
     Log.d("AF_QA", "[AF_QA][DEEPLINK_NATIVE] onNewIntent: ${intent.data}")
     setIntent(intent)
-    // Forward to SDK before onResume stamps the URI with af_consumed=true.
-    // Without this, warm-app VIEW intents are silently consumed and the
-    // registered DeepLinkListener never fires.
-    val url = intent.data?.toString()
-    if (url != null) {
-      AppsFlyerLib.getInstance().performDeepLinking(url, true)
-    }
   }
 }
